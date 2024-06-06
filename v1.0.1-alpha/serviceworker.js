d660f4beb6d96401097dee380cf7262afc007903
Niclas Rådström <niclas@skorpion.se>
2024-06-06 18:13:01 +0200

    Testing v1.0.1-alpha

--- v1.0.1-alpha/serviceworker.js
+++ v1.0.1-alpha/serviceworker.js
@@ -87,7 +87,7 @@ const appShellFiles = [
 	"./v1.0.1-alpha/images/icons/favicon-32x32.png"
 ];
 
-
+/*
 self.addEventListener("install", (event) => {
 	event.waitUntil(
 		caches
@@ -107,19 +107,20 @@ self.addEventListener("install", (event) => {
 		),
 	);
 });
+*/
 
-/*
 self.addEventListener("install", (e) => {
-	//console.log("[Service Worker] Install");
+	console.log("[Service Worker] Install");
+/*
 	e.waitUntil(
 		(async () => {
 			const cache = await caches.open(cacheName);
-			//console.log("[Service Worker] Caching all: app shell and content");
+			console.log("[Service Worker] Caching all: app shell and content");
 			await cache.addAll(contentToCache);
 		})(),
 	);
-});
 */
+});
 
 self.addEventListener("fetch", (e) => {
 	e.respondWith(
