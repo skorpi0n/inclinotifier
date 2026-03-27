9a9259e90f3a0c6784eb1e4304256c4a1d07e3ba
Niclas Rådström <niclas@skorpion.se>
2026-03-27 20:45:57 +0100

    Test

--- v1.0.2-alpha/frontend.js
+++ v1.0.2-alpha/frontend.js
@@ -594,8 +594,8 @@ try{
 		$("subscribe-notif-btn").classList.remove("pulse");
 
 		$("debug").innerHTML += "<span>&gt;frontend.js neither is_iOS() or is_android()</span>";
-		console.log("initserviceworker");
-		initServiceWorker();
+		//console.log("initserviceworker");
+		//initServiceWorker();
 	}
 	$("debug").innerHTML += "<span>&gt;frontend.js was loaded to the end</span>";
 }
